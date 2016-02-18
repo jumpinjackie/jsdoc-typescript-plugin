a777d95165453c2facb2f3ed92c5f05c65b4e50a
Jackie Ng <jumpinjackie@gmail.com>
2016-02-19 00:19:00 +1100

    Add more random JS examples, primarily to help us flesh out the documentation (#33), but also to make sure our plugin is doing what is advertised

---
+++ examples/misc/generics.js
@@ -0,0 +1,30 @@
+/**
+ * @constructor
+ * @classdesc 
+ * A generic class
+ * @template T
+ */
+Foo = function() { };
+
+/**
+ * @description A generic method
+ * @template TArg
+ * @param {TArg} arg An argument of a generic type
+ * @return {string} A string value
+ */
+Foo.prototype.bar = function(arg) { return "foo"; };
+
+/**
+ * @description Another generic method
+ * @template TArg
+ * @param {T} arg The class generic type
+ * @param {TArg} arg2 An argument of a generic type
+ * @return {string} A string value
+ */
+Foo.prototype.setBar = function(arg, arg2) { };
+
+/**
+ * @description Yet another generic method
+ * @return {T} THe class generic type
+ */
+Foo.prototype.getBar = function() { return null; };
