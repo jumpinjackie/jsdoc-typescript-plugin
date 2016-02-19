2522d88185a4f64943382767a8cb55dd3d1a1b12
Jackie Ng <jumpinjackie@gmail.com>
2016-02-19 20:44:36 +1100

    Add missing options argument example

---
+++ examples/misc/optionargs.js
@@ -0,0 +1,10 @@
+/**
+ * @constructor
+ * @classdesc 
+ * A class that takes an options argument
+ * @param {Object} options Object with the following properties:
+ * @param {String} options.url The URL of the service.
+ * @param {String} [options.token] The authorization token to use to connect to the service.
+ * @param {Object} [options.proxy] A proxy to use for requests. This object is expected to have a getURL function which returns the proxied URL, if needed.
+ */
+Optionable = function(options) { };
