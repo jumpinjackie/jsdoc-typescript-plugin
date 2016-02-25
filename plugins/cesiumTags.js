3b8127b56cb515bc97239a57767214194ba7424e
Jackie Ng <jumpinjackie@gmail.com>
2016-02-26 02:09:45 +1100

    Add cesiumTags plugin to match their official JSDoc configuration

---
+++ plugins/cesiumTags.js
@@ -0,0 +1,30 @@
+exports.defineTags = function(dictionary) {
+    dictionary.lookUp('class').synonym('internalConstructor');
+
+    dictionary.defineTag('glsl', {
+        onTagged : function(doclet, tag) {
+            doclet.addTag('kind', 'glsl');
+            doclet.filename = doclet.name;
+        }
+    }).synonym('glslStruct').synonym('glslUniform').synonym('glslConstant').synonym('glslFunction');
+
+    dictionary.defineTag('performance', {
+        mustHaveValue : true,
+        onTagged : function(doclet, tag) {
+            if (!doclet.performance) {
+                doclet.performance = [];
+            }
+            doclet.performance.push(tag.value);
+        }
+    });
+
+    dictionary.defineTag('demo', {
+        mustHaveValue : true,
+        onTagged : function(doclet, tag) {
+            if (!doclet.demo) {
+                doclet.demo = [];
+            }
+            doclet.demo.push(tag.value);
+        }
+    });
+};
