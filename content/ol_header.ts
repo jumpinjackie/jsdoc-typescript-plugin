d97f1043759007cfd44a88bea4d9a1a82b009854
Jackie Ng <jumpinjackie@gmail.com>
2016-07-12 02:00:49 +1000

    Update ol baseline to 3.17.1

--- content/ol_header.ts
+++ content/ol_header.ts
@@ -1,4 +1,4 @@
-// Type definitions for OpenLayers v3.13.1
+// Type definitions for OpenLayers v3.17.1
 // Project: http://openlayers.org/
 // Definitions by: Jackie Ng <https://github.com/jumpinjackie>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
