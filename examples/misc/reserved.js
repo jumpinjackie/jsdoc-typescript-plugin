12c3019b25cdb7ec8c3059ced27b246abfbba8c1
Jackie Ng <jumpinjackie@gmail.com>
2016-12-13 01:07:07 +1100

    Add reserved member name example case

---
+++ examples/misc/reserved.js
@@ -0,0 +1,16 @@
+/**
+ * A class that contains reserved member names
+ * 
+ * @constructor
+ */
+ReservedType = function() { }
+
+/**
+ * Deletes an object by name
+ * 
+ * @param {string} name description
+ * @returns {boolean} Whether any entry with this name was deleted.
+ */
+ReservedType.prototype["delete"] = function(name) {
+
+}
